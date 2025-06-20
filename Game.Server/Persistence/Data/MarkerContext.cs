c50cbfaae7cc2a820cf96337cb07f7cf78d871b1
Toothless <hyunjinney@naver.com>
2025-06-20 17:51:00 +0900

    [Feature] EF Core 설치
    - Entity Framework Core 설치
    - 필요한 기초 클래스 제작 중

---
+++ Game.Server/Persistence/Data/MarkerContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence.Models;
+
+namespace Persistence.Data
+{
+    class MarkerContext : DbContext
+    {
+        public MarkerContext(DbContextOptions<MarkerContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Marker> Markers => Set<Marker>();
+    }
+}
---
+++ Game.Server/Persistence/Models/Marker.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+namespace Persistence.Models
+{
+    public enum MarkerType
+    {
+        DropItem, Clue, SelfClue, Decoration, Trap
+    }
+
+    public enum MarkerSpawnType
+    {
+        Base, OnClose
+    }
+
+    class Marker
+    {
+        public string name;
+        public int dropItemID;
+        public int acquireStep;
+        public int removeStep;
+        public Vector3 position;
+        public Quaternion rotation;
+        public MarkerSpawnType markerSpawnType = MarkerSpawnType.Base;
+        public MarkerType markerType = MarkerType.DropItem;
+    }
+}
